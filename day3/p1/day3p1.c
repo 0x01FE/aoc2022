335ecc8556a9dbe3a417c118285fc17ff76984f1
JISAUAY <jackson.d.hodge@jbhunt.com>
2023-03-07 15:48:27 -0600

    finished day3p1

--- day3/p1/day3p1.c
+++ day3/p1/day3p1.c
@@ -6,6 +6,7 @@
 A-Z : 65-90 ; - 38
 a-z : 97-122 ; - 96
 */
+
 void print_line(char line[], int start, int end) {
     for (int i = start; i < end; i++) {
         printf("%c", line[i]);
@@ -18,12 +19,12 @@ int main() {
     FILE *f;
     char *line = malloc(256);
     int total = 0;
+    int priority = 0;
 
     f = fopen("input.txt", "r");
 
     while (fgets(line, 256, f) != NULL) {
-        int priority = 0;
-        int size = strlen(line) - 1; // -1 because it includes the escape character
+        int size = strlen(line);
         int c1e = size/2; // compartment 1 end
         int c2s = c1e; //compartment 2 start
         char shared = ' ';
@@ -54,9 +55,9 @@ int main() {
         int n = (int) shared;
 
         if (n >= 65 && n <= 90) {
-            priority += n-38;
+            priority = n-38;
         } else if (n >= 97 && n <= 122) {
-            priority+= n-96;
+            priority = n-96;
         }
 
         total += priority;
