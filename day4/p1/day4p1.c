7c920fc8ab46c5e355aa63d0bb0bd66bdf7a74e8
0x01FF <JacksonDHodge@gmail.com>
2023-03-09 12:28:29 +0000

    commit before my laptop runs out of battery

--- day4/p1/day4p1.c
+++ day4/p1/day4p1.c
@@ -18,6 +18,16 @@ bool IntRangeInIntRange(int a, int b, int c, int d)
 
 int main()
 {
+    FILE *File;
+
+    File = fopen("input.text", "r");
+
+    char *Line = malloc(256);
+
+    while(fgets(Line, 256, File) != NULL)
+    {
+
+    }
 
 
 
