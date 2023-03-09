a74f970105a1b7942c5b7f4a26f0a73a71306915
JISAUAY <jackson.d.hodge@jbhunt.com>
2023-03-09 17:57:29 -0600

    Did more with day4 problem 1

--- day4/p1/day4p1.c
+++ day4/p1/day4p1.c
@@ -14,23 +14,72 @@ bool IntRangeInIntRange(int a, int b, int c, int d)
 }
 
 
-
-
 int main()
 {
     FILE *File;
 
-    File = fopen("input.text", "r");
+    File = fopen("test.text", "r");
 
     char *Line = malloc(256);
 
+    int Total = 0;
+
+
     while(fgets(Line, 256, File) != NULL)
     {
+        int ElfRanges[4] = {0, 0, 0, 0};
 
+        char *Elves[2] = malloc(256);
+
+        // Splits the line by the comma in the middle of the line to get each elf's range
+        char *Elf = strtok(Line, ",");
+        Elves[0] = Elf;
+
+        int i = 1;
+        while (Elf != NULL)
+        {
+            Elves[i] = strok(NULL, ",");
+            i++;
+        }
+
+        // Loop through both elves
+        for (int a = 0; a < 2; a++)
+        {
+
+            printf(Elves[a]);
+            printf("\n");
+
+            char *Task = strtok(Elves[a], "-");
+
+            while (Task != NULL)
+            {
+
+                for (int i = 0; i < (sizeof ElfRanges / sizeof (ElfRanges[0])); i++)
+                {
+                    if (ElfRanges[i] == 0)
+                    {
+                        ElfRanges[i] = (int) strtol(Task, NULL, 10);
+                        break;
+                    }
+                }
+
+                Task = strtok(NULL, "-");
+
+            }
+
+        }
+
+        printf("Elf Pair Ranges: ");
+        for (int i = 0; i < (sizeof ElfRanges / sizeof (ElfRanges[0])); i++)
+        {
+            printf("%d ", ElfRanges[i]);
+        }
+
+        printf("\n");
     }
 
 
-
+    fclose(File);
 
     return 0;
 }
