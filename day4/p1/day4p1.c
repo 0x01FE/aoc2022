1e9017789751618bfc9664d553d75e1e40819e72
0x01FF <JacksonDHodge@gmail.com>
2023-03-09 12:26:07 +0000

    Started day4 problem 1

---
+++ day4/p1/day4p1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+bool IntRangeInIntRange(int a, int b, int c, int d)
+{
+    if (c <= a && d >= b)
+        return true;
+
+    else
+        return false;
+}
+
+
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
