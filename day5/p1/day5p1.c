4a296afe3f91b6dae7f76caadf7437d54f573881
JISAUAY <jackson.d.hodge@jbhunt.com>
2023-03-13 17:58:03 -0500

    Git wants e to commit, I want to sleep. Take me sweet void.

--- day5/p1/day5p1.c
+++ day5/p1/day5p1.c
@@ -1,31 +1,58 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 
 
+char * GetLine(char *FileName, int LineNumber)
+{
+    int CurrentLineNumber = 1;
 
+    FILE *File;
+    File = fopen(FileName, "r");
+    char * Line = malloc(256);
 
+    while (fgets(Line, 256, File) != NULL)
+    {
+
+        if (CurrentLineNumber == LineNumber)
+            return Line;
+
+        else
+            CurrentLineNumber++;
+
+    }
+
+    fclose(File);
+
+}
 
 int main()
 {
+
     char Filename[] = "test.text";
 
-    char *String = malloc(5);
+    char * Line = malloc(256);
+    int EndOfDrawing;
 
-    strcpy(String, "Test");
+    int CurrentLine = 1;
+    while (true)
+    {
 
-    size_t StringSize = strlen(String) + 1;
-    printf("%d\n", StringSize);
+        Line = GetLine(Filename, CurrentLine);
 
+        if (isdigit(Line[2]))
+        {
+            EndOfDrawing = CurrentLine;
+            break;
+        }
+        else
+            CurrentLine++;
 
-    String = realloc(String, 6);
-
-    printf(strcat(String, "s"));
-
-
-
-    free(String);
+    }
 
     return 0;
+
 }
