9c6a5c6ff212d2116b2ac2b613afec9c27e81d9e
0x01FF <JacksonDHodge@gmail.com>
2023-03-15 11:31:29 +0000

    did more on day5 p1

--- day5/p1/day5p1.c
+++ day5/p1/day5p1.c
@@ -71,7 +71,35 @@ int main()
         Collumn Number Line - 1 should match the number of lines it took to get to
         the bottom of the drawing which we can then infer the max number of items in one stack with.
     */
-    char * Stacks[Collumns][CollumnNumberLine - 1];
+    char * Stacks[Collumns];
+
+    for (int i = 0; i < Collumns; i ++)
+    {
+        Stacks[i] = malloc(Collumns);
+    }
+
+
+    int CurrentCollumn = 1;
+    int Place;
+    char * CollumnContent;
+    char Temp[1];
+
+    while (CurrentCollumn <= Collumns)
+    {
+        GetLine(FileName, CurrentLine, Line);
+        Place = (CurrentCollumn * 2) - 1;
+
+        // Roundabout method for making that character a string
+        Temp[0] = Line[Place];
+
+        if (Stacks[CurrentCollumn] != NULL)
+            strcat(Stacks[CurrentCollumn], Temp);
+
+        else
+            Stacks[CurrentCollumn] = Temp;
+
+        CurrentCollumn++;
+    }
 
 
 
