34a8b6814ad627d5f669d9c305eefa3ed265ec22
0x01FF <thevaulttecrep@gmail.com>
2023-06-24 14:06:43 -0500

    more work on day5p1

--- day5/p1/day5p1.c
+++ day5/p1/day5p1.c
@@ -10,6 +10,24 @@ struct Node{
 };
 
 
+int CountLines(char *FileName)
+{
+    char *Line = malloc(256);
+    int LineCount = 0;
+
+    FILE *File;
+    File = fopen(FileName, "r");
+
+    while (fgets(Line, 256, File) != NULL)
+    {
+        LineCount++;
+    }
+
+    fclose(File);
+
+    return LineCount;
+}
+
 
 void GetLine(char *FileName, int LineNumber, char* Line)
 {
@@ -40,7 +58,7 @@ void GetLine(char *FileName, int LineNumber, char* Line)
 int main()
 {
 
-    char FileName[] = "input.text";
+    char FileName[] = "test.text";
 
     int CollumnNumberLine;
 
@@ -72,10 +90,11 @@ int main()
     Collumns = strlen(Line) / 4;
     printf("Collumns : %d\n\n", Collumns);
 
-    /*
-        Collumn Number Line - 1 should match the number of lines it took to get to
-        the bottom of the drawing which we can then infer the max number of items in one stack with.
-    */
+
+
+
+
+    // Parsing the starting positions
     struct Node Stacks[Collumns];
 
     for (int i = 0; i < Collumns; i++)
@@ -101,15 +120,14 @@ int main()
 
             ContainerLetter = Line[Place];
 
+            if (ContainerLetter == ' ')
+                break;
+
             struct Node * Current = &Stacks[CurrentCollumn];
 
             while (Current->Next != NULL)
-            {
                 Current = Current->Next;
-            }
 
-            if (ContainerLetter == ' ')
-                break;
 
             if (Current->Letter == '0')
             {
@@ -147,8 +165,73 @@ int main()
 
 
 
+    // Parsing the movements
+    CurrentLine = CollumnNumberLine + 2; // Line where movements start
+
+    int Lines = CountLines(FileName);
+
+    while (CurrentLine <= Lines)
+    {
+        GetLine(FileName, CurrentLine, Line);
+
+        int CurrentToken = 0; // Delimiter Token
+
+        // The numbers in the movement line, move x number of crates from a to b
+        int Number; // Number of containers to move
+        int Src; // Source
+        int Dest; // Destination
+
+        char * Token = strtok(Line, " ");
+
+        while (Token != NULL)
+        {
+            if (CurrentToken == 1)
+            {
+                printf("Amount is %s\n", Token);
+                Number = atoi(Token);
+            }
+            else if (CurrentToken == 3)
+            {
+                printf("Source is %s\n", Token);
+                Src = atoi(Token) - 1;
+            }
+            else if (CurrentToken == 5)
+            {
+                printf("Destination is %s\n", Token);
+                Dest = atoi(Token) - 1;
+            }
 
 
+            Token = strtok(NULL, " ");
+
+            CurrentToken++;
+        }
+
+
+        // Containers that are being moved
+        struct Node * TempContainers[Number];
+
+        // Take x off of a
+        for (int i = 0; i < Number; i++)
+        {
+            struct Node * Current = &Stacks[Src];
+
+            while (Current->Next != NULL)
+            {
+                if (Current->Next == NULL)
+                {
+                    TempContainers[i] = Current->Next;
+                    Current->Next = NULL;
+                }
+                Current = Current->Next;
+            }
+
+            printf("%c\n", TempContainers[i]->Letter);
+
+        }
+
+        CurrentLine++;
+    }
 
 
 
